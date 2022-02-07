d875602a039741ccb12f903f006621cab6e1e566
MacLier <75802094+MacLier@users.noreply.github.com>
2022-02-07 12:25:36 +0100

    feat: base server and first middleware works

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-const app = require('express');
+const express = require('express');
+
+const app = express();
+
+
+app.use((req, res, next) => {
+    console.log('In the middleware.');
+    res.send('<h1>Basic dummy</h1>')
+});
+
+app.listen(3000);
