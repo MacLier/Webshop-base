da367648ecf29a44663579899eb21c4d9830be45
MacLier <75802094+MacLier@users.noreply.github.com>
2022-02-07 19:36:23 +0100

    Feat: dynamic content works

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path')
 
-const adminRoutes = require('./routes/admin');
+const app = express();
+
+app.set('view engine', 'pug');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const pageNotFound = require('./routes/404PageNotFound')
 
-const app = express();
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 app.use('*', pageNotFound)
 
