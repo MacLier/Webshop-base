f83ca94ad9ad27c759469d12092f55d5a468d2f5
MacLier <75802094+MacLier@users.noreply.github.com>
2022-02-07 15:39:15 +0100

    feat: outsourceing path to /util

--- routes/404PageNotFound.js
+++ routes/404PageNotFound.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('', (req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, '..', 'views', 'pageNotFound.html'))
+    res.status(404).sendFile(path.join(rootDir, 'views', 'pageNotFound.html'))
 })
 
 module.exports = router;
