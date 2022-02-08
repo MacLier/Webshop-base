4e9224988ec7b67873be2b20db5c49b28d2dc276
MacLier <75802094+MacLier@users.noreply.github.com>
2022-02-08 21:35:13 +0100

    feat: product model refactored

--- models/product.js
+++ models/product.js
@@ -1,6 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
+
+const getProductsFromFile = (cb) => {
+    fs.readFile(p, (err, data) => {
+        if (err) {
+            return cb([]);
+        }
+        cb(JSON.parse(data));
+    })
+}
+
+const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
 module.exports = class Product {
 
     constructor(title) {
@@ -9,28 +20,16 @@ module.exports = class Product {
     }
 
     save() {
-        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
-        fs.readFile(p, (err, data) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(data)
-            }
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
-            })
-        })
-    }
-
-    static fetchAll(cb) {
-        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+            });
+        });
         fs.readFile(p, (err, data) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(data));
-        })
-
+        });
     }
-
-}
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
+    }
+};
