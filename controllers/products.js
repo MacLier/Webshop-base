9afd4e3a736a4f3410ee0ed257d3775ff7bf29ca
MacLier <75802094+MacLier@users.noreply.github.com>
2022-02-09 14:54:15 +0100

    feat: cart/checkout/index/product-detail/edit-product/products templates added

--- controllers/products.js
+++ controllers/products.js
@@ -1,18 +1,19 @@
 const Product = require("../models/product");
 
 exports.getAddProductPage = (req, res, next) => {
-    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+    res.render('admin/add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 }
 
 exports.postAddProduct = (req, res, next) => {
     const product = new Product(req.body.title);
+    console.log(req.body);
     product.save()
     res.redirect('/');
 }
 
 exports.getProducts = (req, res, next) => {
     const products = Product.fetchAll((products) => {
-        res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' })
+        res.render('user/product-list', { prods: products, pageTitle: 'Shop', path: '/' })
 
     });
 }
