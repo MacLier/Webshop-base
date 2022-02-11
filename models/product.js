b6fea030803b7cc0c981deb405ed67c002661287
MacLier <75802094+MacLier@users.noreply.github.com>
2022-02-11 13:05:29 +0100

    bugfix: add to cart function works in detail page

--- models/product.js
+++ models/product.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
 
 const getProductsFromFile = cb => {
     fs.readFile(p, (err, data) => {
