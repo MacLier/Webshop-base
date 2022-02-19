87321a8a82eaaefadcdaaf6de79fce307ec48415
MacLier <75802094+MacLier@users.noreply.github.com>
2022-02-19 16:05:37 +0100

    feat: addToCart to user model

--- models/user.js
+++ models/user.js
@@ -4,9 +4,10 @@ const mongoDb = require('mongodb');
 
 
 class User {
-    constructor(username, email) {
+    constructor(username, email, cart) {
         this.username = username;
         this.email = email;
+        this.cart = cart;
         this._id = id ? new mongoDb.ObjectId(id) : null;
     }
     save() {
@@ -17,6 +18,17 @@ class User {
             .catch(err => {
                 console.log(err);
             });
+    }
+    addToCart(product) {
+        const cartProducts = this.cart.items.findIndex(cp => {
+            return cp._id === product._id;
+        });
+        const updatedCart = { items: [{ ...product, quantity: 1 }] };
+        const db = getDb();
+        return db.collection('users')
+            .updateOne({ id: new mongoDb.ObjectId(this._id) },
+                { $set: { cart: updatedCart } }
+            );
     }
     static findById(userId) {
         const db = getDb();
