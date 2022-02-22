1a0e0352fad12b509b8be3d2a59d628d568ec8bb
MacLier <75802094+MacLier@users.noreply.github.com>
2022-02-22 18:43:48 +0100

    feat: session deleted and logout view created

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,3 +16,9 @@ exports.postLogin = (req, res, next) => {
         })
         .catch(err => console.log(err));
 }
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        console.log(err);
+        res.redirect('/');
+    });
+}
