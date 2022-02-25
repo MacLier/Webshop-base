ee1c5fe227fa2349cf27c2eb5f1b19dbe162e86c
MacLier <75802094+MacLier@users.noreply.github.com>
2022-02-25 15:06:43 +0100

    feat: using middleware to protect routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,17 @@
 const express = require('express');
 
-const adminController = require('../controllers/admin')
+const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
 
 
 const router = express.Router();
 
-router.get('/add-product', adminController.getAddProduct);
-router.get('/edit-product/:id', adminController.getEditProduct);
-router.get('/products', adminController.getAdminProducts);
+router.get('/add-product', isAuth, adminController.getAddProduct);
+router.get('/edit-product/:id', isAuth, adminController.getEditProduct);
+router.get('/products', isAuth, adminController.getAdminProducts);
 
-router.post('/add-product', adminController.postAddProduct);
-router.post('/edit-product', adminController.postEditProduct);
-router.post('/delete-product', adminController.postDeleteProduct);
+router.post('/add-product', isAuth, adminController.postAddProduct);
+router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,17 +1,18 @@
 const express = require('express');
 
 const shopController = require('../controllers/shop');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 
 router.get('/', shopController.getIndex);
-router.get('/cart', shopController.getCart);
-router.post('/cart', shopController.postCard);
-router.post('/cart-delete-item', shopController.postCardDeleteProduct);
-router.get('/orders', shopController.getOrders);
-router.post('/create-order', shopController.postOrder);
-// router.get('/checkout', shopController.getCheckout);
+router.get('/cart', isAuth, shopController.getCart);
+router.post('/cart', isAuth, shopController.postCard);
+router.post('/cart-delete-item', isAuth, shopController.postCardDeleteProduct);
+router.get('/orders', isAuth, shopController.getOrders);
+router.post('/create-order', isAuth, shopController.postOrder);
+// router.get('/checkout', isAuth, shopController.getCheckout);
 router.get('/products', shopController.getProducts);
 router.get('/products/:id', shopController.getProduct);
 
