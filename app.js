853633bfdc21623419930d06da93492c0103529c
MacLier <75802094+MacLier@users.noreply.github.com>
2022-02-26 15:31:54 +0100

    feat: adding CSRF protection

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const path = require('path');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 
-// const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb+srv://testadmin:awCWUmGv8Doz6Ph1@parachute.pecmz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const MONGODB_URI = 'mongodb+srv://testadmin:awCWUmGv8Doz6Ph1@parachute.pecmz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 const app = express();
 const store = new MongoDBStore({
     uri: MONGODB_URI,
     collection: 'sessions',
-
 });
+
+const csrfProtection = csrf();
 
 app.set('view engine', 'pug');
 
@@ -28,7 +29,8 @@ const pageNotFound = require('./routes/404PageNotFound');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'bigsecret:D', resave: false, saveUninitialized: false, store: store }))
+app.use(session({ secret: 'bigsecret:D', resave: false, saveUninitialized: false, store: store }));
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
 
@@ -42,6 +44,12 @@ app.use((req, res, next) => {
         })
         .catch(err => console.log(err));
 });
+
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+})
 
 app.use(authRoutes);
 app.use('/admin', adminRoutes);
