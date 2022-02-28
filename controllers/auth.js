ac64639067bae27d833eda3be3657730de0cd23a
MacLier <75802094+MacLier@users.noreply.github.com>
2022-02-28 18:00:28 +0100

    bugfix:  get requests message cont to let, nodemailer uninstall

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 
+
 exports.getLogin = (req, res, next) => {
-    const message = req.flash('error');
+    let message = req.flash('error');
     if (message.length > 0) {
         message = message[0];
     } else {
@@ -46,7 +47,7 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
-    const message = req.flash('error');
+    let message = req.flash('error');
     if (message.length > 0) {
         message = message[0];
     } else {
